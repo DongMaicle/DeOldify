f9a6f039eae35f45b663bff86dafe7b494bf6271
DongMaicle <128656123+DongMaicle@users.noreply.github.com>
2023-05-01 19:36:30 +0800

    Create colorize_video.py

---
+++ deoldify/colorize_video.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+
+import cv2
+import torch
+import torchvision.transforms as T
+
+from model import Generator
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--video_path', type=str, required=True, help='path to the input video')
+parser.add_argument('--output_path', type=str, required=True, help='path to save the output video')
+parser.add_argument('--model_path', type=str, default='models/la_muse.pth', help='path to the pretrained model')
+args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load model
+model = Generator().to(device)
+state_dict = torch.load(args.model_path)
+model.load_state_dict(state_dict)
+model.eval()
+
+# Define image pre-processing
+transform = T.Compose([
+    T.Resize(256),
+    T.CenterCrop(256),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
+])
+
+# Open input video
+cap = cv2.VideoCapture(args.video_path)
+
+# Get video properties
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# Define video writer
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(args.output_path, fourcc, fps, (width, height))
+
+with torch.no_grad():
+    # Process each frame of the video
+    for i in range(total_frames):
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert frame to tensor
+        img = transform(frame).unsqueeze(0).to(device)
+
+        # Apply style transfer
+        stylized_img = model(img)
+
+        # Convert tensor back to image
+        stylized_img = (stylized_img.squeeze(0).cpu().detach().clamp_(0, 1).numpy() * 255).astype('uint8')
+        stylized_img = cv2.cvtColor(stylized_img, cv2.COLOR_RGB2BGR)
+
+        # Write frame to output video
+        out.write(stylized_img)
+
+        # Print progress
+        print(f'Processed frame {i + 1}/{total_frames}')
+
+        # Release memory
+        del img
+        del stylized_img
+
+    # Release resources
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+print('Done!')
